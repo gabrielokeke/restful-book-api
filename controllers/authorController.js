6203b83f10b43fbd484c35241dc27c6036c6858f
gabrielokeke <okekegabriel84@gmail.com>
2025-03-25 09:01:08 +0100

    Update authorController.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -13,13 +13,10 @@ exports.createAuthor = async (req, res) => {
   }
 };
 
-// Retrieve all authors
-const Author = require('../models/author');
-
 // Get all authors
 exports.getAllAuthors = async (req, res) => {
   try {
-    const authors = await Author.find(); // Fetch all authors
+    const authors = await Author.find();
     res.status(200).json(authors);
   } catch (err) {
     res.status(500).json({ message: 'Erreur lors de la récupération des auteurs', error: err });
